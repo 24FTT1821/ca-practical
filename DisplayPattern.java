fd919eb3a4f78b801089ac3329aa0301b529d5d7
24ftt1821 <220729493+24FTT1821@users.noreply.github.com>
2025-08-16 17:49:55 +0800

    Delete DisplayPattern.java

--- DisplayPattern.java
+++
@@ -1,8 +0,0 @@
-public class DisplayPattern {
-    public static void main (String[] ags){
-        System.out.println("(\\__/)  I am an");
-        System.out.println("( @w@)         OOO   W     W L");
-        System.out.println("/) w )        O   O   W W W  L");
-        System.out.println(" ^  ^          OOO     W W   L L L");
-    }
-}
