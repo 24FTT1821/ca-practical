c62b3f806dec66c1c56e22ef4929d45fc1e134f7
24ftt1821 <220729493+24FTT1821@users.noreply.github.com>
2025-08-16 17:50:36 +0800

    Delete DisplayTwoMessages.java

--- DisplayTwoMessages.java
+++
@@ -1,6 +0,0 @@
-public class DisplayTwoMessages {
-    public static void main (String[] ags) {
-        System.out.println("Welcome to AD2204 Introduction to Programming");
-        System.out.println("This is Practical 2");
-    }
-}
