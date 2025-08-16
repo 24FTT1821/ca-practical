be86781212ee823552f9aff6d8ed8a8ca4252837
24ftt1821 <220729493+24FTT1821@users.noreply.github.com>
2025-08-16 17:57:06 +0800

    Practical 2

---
+++ Practical_2/ComputeExpression2.java
@@ -0,0 +1,13 @@
+package Practical_2;
+public class ComputeExpression2 {
+    public static void main (String[] ags){
+        // Calculate the expression , DOuble for calculate with decimals
+        double result = (7.5 * 2.5 - 1.5 * 3) / (4 - 2.5);
+
+        //Display the formatted output
+         System.out.println("The result of 7.5 x 2.5 - 1.5 x 3");
+         System.out.println("              ------------------- is "+result);
+         System.out.println("                   4 - 2.5");
+    
+     }
+}
---
+++ Practical_2/DisplayTwoMessages.java
@@ -0,0 +1,7 @@
+package Practical_2;
+public class DisplayTwoMessages {
+    public static void main (String[] ags) {
+        System.out.println("Welcome to AD2204 Introduction to Programming");
+        System.out.println("This is Practical 2");
+    }
+}
