7568dd53fe9013d9ed421d58887cc4fcb208fd35
24ftt1821 <220729493+24FTT1821@users.noreply.github.com>
2025-08-27 21:51:24 +0800

    Update Reverse5Letters_5_2.java

--- Practical_5/Reverse5Letters_5_2.java
+++ Practical_5/Reverse5Letters_5_2.java
@@ -6,21 +6,25 @@ public class Reverse5Letters_5_2 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         
-
+             // Step 1: Ask user for 5-letter word
         System.out.print("Enter the 5 letters word: ");
-        String word = input.nextLine(); 
-        // stores the word entered by user
-
-        if (word.length() == 5) { 
-            //StringBuilder helps reverse the characters
-            String reversed = new StringBuilder(word).reverse().toString(); 
-            //reversed word
-            System.out.println("The reverse of the word " + word + " is " + reversed);
-        } else {
-            System.out.println("Error: Word must be exactly 5 characters long.");
-        }
+        String word = input.next();
         
-        //close the scanner to free resources
-        input.close(); 
+        // Check if word length is exactly 5
+        if (word.length() != 5) {
+            System.out.println("Error: The word must be exactly 5 letters.");
+            
+        } else {
+            // Step 2: Reverse using charAt()
+            String reverse = "";
+            for (int i = word.length() - 1; i >= 0; i--) {
+                reverse += word.charAt(i);
+            }
+
+            // Step 3: Output the reverse word
+            System.out.println("The reverse of the word " + word + " is " + reverse);
+        }
+
+        input.close();
     }
 }
